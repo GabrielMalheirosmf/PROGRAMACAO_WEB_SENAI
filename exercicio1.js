fc1014aaf91d50ddd04985e119d0d18ed87bacd7
Gabriel Malheiros <100000563+GabrielMalheirosmf@users.noreply.github.com>
2022-03-04 18:53:14 -0300

    exercício

---
+++ exercicio1.js
@@ -0,0 +1,48 @@
+//alert("EU TO DE BOA!");
+// Declarar uma variável chamada `testex`, sem valor.
+var testex;
+console.log(testex);
+// Após declarada, atribua o valor 10 à variável `testex`.
+testex = 10;
+console.log(testex);
+// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+var soma;
+soma = 15+8;
+console.log(soma)
+// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+    var soma ;
+    soma = soma +1;
+    console.log(soma);
+// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+var soma ;
+soma *= 3;
+// Qual é o valor da variável `soma` até aqui
+console.log(soma);
+// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+ var souninja = true;
+ console.log(souninja);
+// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+    var comida = ['arroz' ,'feijão', 'ovo'];
+// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+var feijao = comida[1];
+console.log(feijao);
+// Digite o código que verifica se a variável `soma' é igual a variável `testex` (testando também o tipo).
+if(soma===testex){
+console.log("são iguais");
+}else{
+  console .log("São diferentes");
+    }
+// Digite o código que verifica se a variável `testex` é menor ou igual à variável `soma`.
+if(soma<=testex){
+    console.log("são iguais");
+    }else{
+      console  .log("é menor ou igual");
+        }
+// Crie uma função chamada `divisao` que receba como parâmetro dois números, e retorne o resultado da divisão entre eles.
+var numero1 =10;
+var numero2 = 2;
+var divisao = numero1 / numero2 ;
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+console.log(divisao)
+
+
